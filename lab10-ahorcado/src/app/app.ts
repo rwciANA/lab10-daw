312cbd817591f585fa95f11c5b9b52242ebca445
rwciANA <rcondorii@unsa.edu.pe>
2026-06-14 22:48:32 -0500

    Implementar lógica de adivinanza y selección aleatoria

--- lab10-ahorcado/src/app/app.ts
+++ lab10-ahorcado/src/app/app.ts
@@ -23,4 +23,43 @@ export class App {
   vidasRestantes = 6;
   juegoTerminado = false;
   gano = false;
+
+  constructor() {
+    this.iniciarJuego();
+  }
+
+  iniciarJuego() {
+    const indice = Math.floor(Math.random() * this.palabras.length);
+    this.palabraSecreta = this.palabras[indice];
+    this.letrasAdivinadas = [];
+    this.letrasIncorrectas = [];
+    this.vidasRestantes = 6;
+    this.juegoTerminado = false;
+    this.gano = false;
+  }
+
+  get palabraOculta(): string {
+    return this.palabraSecreta
+      .split('')
+      .map(letra => this.letrasAdivinadas.includes(letra) ? letra : '_')
+      .join(' ');
+  }
+
+  adivinarLetra(letra: string) {
+    if (this.juegoTerminado) return;
+    if (this.letrasAdivinadas.includes(letra)) return;
+    if (this.letrasIncorrectas.includes(letra)) return;
+
+    if (this.palabraSecreta.includes(letra)) {
+      this.letrasAdivinadas.push(letra);
+      this.gano = this.palabraSecreta
+        .split('')
+        .every(l => this.letrasAdivinadas.includes(l));
+      if (this.gano) this.juegoTerminado = true;
+    } else {
+      this.letrasIncorrectas.push(letra);
+      this.vidasRestantes--;
+      if (this.vidasRestantes === 0) this.juegoTerminado = true;
+    }
+  }
 }
