cd162f9ce62a8d0954756ac597c560d9a2d5b958
rwciANA <rcondorii@unsa.edu.pe>
2026-06-14 22:47:58 -0500

    Agregar lista de 20 palabras tecnológicas

--- lab10-ahorcado/src/app/app.ts
+++ lab10-ahorcado/src/app/app.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  readonly palabras = [
+    'ALGORITMO', 'COMPILADOR', 'PROCESADOR',
+    'VARIABLE', 'SERVIDOR', 'REPOSITORIO',
+    'ENCRIPTACION', 'CIBERSEGURIDAD', 'NAVEGADOR',
+    'ARQUITECTURA', 'INTERFAZ', 'PROTOCOLO',
+    'MEMORIA', 'PROGRAMACION', 'LENGUAJE',
+    'INTELIGENCIA', 'SISTEMA', 'APLICACION',
+    'TECNOLOGIA', 'INFORMATICA'
+  ];
 }
